acddd1ff0b4b89433f76e0183387abb20d21343f
Gianluca Stoob <gianlucas@hispeed.ch>
2020-06-27 10:51:08 +0200

    Kritischer Bug bei den Observers behoben, GMCP plugin ins Unitopia Soundpack eingebunden

--- lua/unitopia/constants.lua
+++ lua/unitopia/constants.lua
@@ -1,6 +1,7 @@
 Constants = {
   Plugins = {
-    AUDIO_PLUGIN_ID = "aedf0cb0be5bf045860d54b7"
+    AUDIO_PLUGIN_ID = "aedf0cb0be5bf045860d54b7",
+    GMCP_PLUGIN_ID = "c074220f28dcae21baaf08e6"
   },
   -- The following table defines constants that are required to successfully send and receive GMCP packets
   -- See https://www.gammon.com.au/gmcp for more info
@@ -8,7 +9,7 @@ Constants = {
     SB   = 0xFA, -- Start of subnegotiation
     SE = 0xF0, -- End of subnegotiation
     IAC  = 0xFF, -- Interpret As Command
-    GMCP = 0xC9 -- GMCP sequence (decimal 201)
+    GMCP = 201 -- GMCP sequence, required to identify incoming and outgoing GMCP packets successfully
   }
 }
 
